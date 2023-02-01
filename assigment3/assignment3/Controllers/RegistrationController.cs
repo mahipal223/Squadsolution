251bfc3b8434729d8f3f2d12fb35119f0e3791d3
mahipal <m29ahipal@gmail.com>
2023-02-01 15:03:19 +0530

    Assignment3 Completed

--- assigment3/assignment3/Controllers/RegistrationController.cs
+++ assigment3/assignment3/Controllers/RegistrationController.cs
@@ -22,10 +22,17 @@ namespace assignment3.Controllers
         {
             if (HttpContext.Session.GetString("loginemail") != null)
             {
+
                 ViewBag.loginSessionEmail = HttpContext.Session.GetString("loginemail");
                 string x = HttpContext.Session.GetString("loginemail");
-                ViewBag.logindata = _db.Registrations.Where(m => m.Email == x);
-                return View();
+                ViewBag.logindata = (_db.Registrations.Where(m => m.Email == x));
+
+                foreach (var items in ViewBag.logindata)
+                {
+                    ViewBag.firstName1 = DecryptString(items.FirstName);
+                    ViewBag.lastName1 = DecryptString(items.LastName);
+                    return View();
+                }
             }
             return RedirectToAction("Login");
         }
@@ -40,8 +47,12 @@ namespace assignment3.Controllers
         [HttpPost]
         public IActionResult Login(Registration obj)
         {
+            obj.Email = EnryptString(obj.Email);
+            obj.Password = EnryptString(obj.Password);
             if (obj.Email != null && obj.Password != null)
+
             {
+
                 var students = _db.Registrations.Where(m => m.Email == obj.Email && m.Password == obj.Password).FirstOrDefault();
                 if (students != null)
                 {
@@ -52,6 +63,11 @@ namespace assignment3.Controllers
             }
             return View();
         }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("loginemail");
+            return RedirectToAction("login");
+        }
         public IActionResult Create()
         {
 
@@ -61,15 +77,15 @@ namespace assignment3.Controllers
         public IActionResult Create(Registration obj)
         {
             Registration rs = new Registration();
-
             if (ModelState.IsValid)
             {
-                rs.FirstName = obj.FirstName;
-                rs.LastName = obj.LastName;
-                rs.Email = obj.Email;
-                rs.Password = obj.Password;
+
+                rs.FirstName = EnryptString(obj.FirstName);
+                rs.LastName = EnryptString(obj.LastName);
+                rs.Email = EnryptString(obj.Email);
+                rs.Password = EnryptString(obj.Password);
                 rs.DateOfBirth = obj.DateOfBirth;
-                rs.ContactNo = obj.ContactNo;
+                rs.ContactNo = EnryptString(obj.ContactNo);
                 rs.DateAdded = DateTime.Now;
                 _db.Registrations.Add(rs);
                 _db.SaveChanges();
@@ -93,6 +109,62 @@ namespace assignment3.Controllers
                 return View();
             }
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            var x = _db.Registrations.Find(id);
+            Registration rs = new Registration();
+            rs.RecId = id;
+            rs.FirstName = DecryptString(x.FirstName);
+            rs.LastName = DecryptString(x.LastName);
+            rs.ContactNo = DecryptString(x.ContactNo);
+            rs.Email = DecryptString(x.Email);
+            rs.Password = DecryptString(x.Password);
+            rs.DateAdded = x.DateAdded;
+            rs.LastUpadated = x.LastUpadated;
+
+            return View(rs);
+        }
+        [HttpPost]
+        public IActionResult Update(Registration obj)
+        {
+
+            Registration rs = new Registration();
+            rs.RecId = obj.RecId;
+            rs.FirstName = EnryptString(obj.FirstName);
+            rs.LastName = EnryptString(obj.LastName);
+            rs.Email = EnryptString(obj.Email);
+            rs.Password = EnryptString(obj.Password);
+            rs.DateOfBirth = obj.DateOfBirth;
+            rs.ContactNo = EnryptString(obj.ContactNo);
+            rs.LastUpadated = DateTime.Now;
+            rs.DateAdded = obj.DateAdded;
+            _db.Registrations.Update(rs);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public string DecryptString(string encrString)
+        {
+            byte[] b;
+            string decrypted;
+            try
+            {
+                b = Convert.FromBase64String(encrString);
+                decrypted = System.Text.ASCIIEncoding.ASCII.GetString(b);
+            }
+            catch (FormatException fe)
+            {
+                decrypted = "" + fe;
+            }
+            return decrypted;
+        }
+
+        public string EnryptString(string strEncrypted)
+        {
+            byte[] b = System.Text.ASCIIEncoding.ASCII.GetBytes(strEncrypted);
+            string encrypted = Convert.ToBase64String(b);
+            return encrypted;
         }
 
     }
